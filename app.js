c4b038c4dcb8ca1de35745fe6fa92734c91eaf1b
gopnikada <klarsky.kirill6@gmail.com>
2021-01-29 22:45:36 +0100

    local storage routine

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express()
 const pg = require('pg')
 const { Pool } = require('pg')
 const jwt = require('jsonwebtoken')
+var LocalStorage = require('node-localstorage').LocalStorage,
+    localStorage = new LocalStorage('./scratch')
 
 let loggedIn = null
 app.set('view engine', 'hbs')
@@ -44,30 +46,7 @@ app.get('/', (req,res)=>{
 })
 
 app.get('/items', async (req,res)=>{
-    // We can obtain the session token from the requests cookies, which come with every request
-    const token = req.cookies.token
 
-    // if the cookie is not set, return an unauthorized error
-    if (!token) {
-        return res.status(401).end()
-    }
-
-    var payload
-    try {
-        payload = jwt.verify(token, jwtKey)
-    } catch (e) {
-        if (e instanceof jwt.JsonWebTokenError) {
-            // if the error thrown is because the JWT is unauthorized, return a 401 error
-            return res.status(401).end()
-        }
-        // otherwise, return a bad request error
-        return res.status(400).end()
-    }
-
-    // Finally, return the welcome message to the user, along with their
-    // username given in the token
-    loggedIn = payload.username
-    //res.send(`Welcome ${payload.username}!`)
     pool.on('error', (err)=>{
         console.error(err)
         process.exit(-1)
@@ -81,7 +60,7 @@ app.get('/items', async (req,res)=>{
             console.log('category: '+req.query.optVal)
             rq = `SELECT * FROM items WHERE cat='${req.query.optVal}' ORDER BY id ASC`
         }
-        console.log(loggedIn.toString())
+
         if ( err ) throw err
         client.query(rq, (err, result)=>{
             done()
@@ -139,49 +118,74 @@ app.get('/items/:id', async (req,res)=>{
 })
 
 app.get('/basket', async (req,res)=>{
-    console.log(loggedIn)
-    pool.on('error', (err)=>{
-        console.error(err)
-        process.exit(-1)
-    })
-    pool.connect((err, client, done)=>{
-        if ( err ) throw err
-        client.query(`SELECT orders.id, orders.itemId, orders.count, items.name, items.image
-        FROM orders
-        JOIN items ON items.id=orders.itemId`, (err, result)=>{
-            done()
-            if (err){
-                console.log(err.stack)
-            }else{
-                console.log(result.rows)
-                res.render('basket', {layout: 'mainLayout', orders: result.rows, loggedIn: loggedIn})
-            }
-        })
-    })
-})
+    // console.log(loggedIn)
+    // pool.on('error', (err)=>{
+    //     console.error(err)
+    //     process.exit(-1)
+    // })
+    // pool.connect((err, client, done)=>{
+    //     if ( err ) throw err
+    //     client.query(`SELECT orders.id, orders.itemId, orders.count, items.name, items.image
+    //     FROM orders
+    //     JOIN items ON items.id=orders.itemId`, (err, result)=>{
+    //         done()
+    //         if (err){
+    //             console.log(err.stack)
+    //         }else{
+    //             console.log(result.rows)
+    //             res.render('basket', {layout: 'mainLayout', orders: result.rows, loggedIn: loggedIn})
+    //         }
+    //     })
+    // })
+    // let allOrders = []
+    // console.log('ls' + localStorage.getItem(`order1`))
+    // console.log('ls' + localStorage.getItem(`order2`))
+    // console.log('ls' + localStorage.getItem(`order3`))
 
+    var arrayOfValues = Object.values(localStorage);
+
+
+    res.render('basket', {
+        layout: 'mainLayout',
+        orders: arrayOfValues[8],
+        loggedIn: loggedIn
+    })
+
+})
+let n=0
 app.post('/basket', async (req,res)=>{
 
-        pool.connect((err, client, done)=>{
-            if(err) return console.error(err)
 
-            client.query(`                 
-                 UPDATE items
-                SET count = count-$2    
-                WHERE id=$1;
-                `,[req.body.getIdInput, req.body.getCountInput] )
-            client.query(`               
-                 INSERT INTO orders (itemId, count, userName) values
-                ( (SELECT id FROM items WHERE id=$1), $2, '${loggedIn}')
-                 on conflict (itemId) DO
-                 UPDATE SET
-                 count = orders.count + $2
-                 returning *;
-                 `,
-                [req.body.getIdInput, req.body.getCountInput])
-            done()
-            res.redirect("/basket")
-        })
+        // pool.connect((err, client, done)=>{
+        //     if(err) return console.error(err)
+        //
+        //     client.query(`
+        //          UPDATE items
+        //         SET count = count-$2
+        //         WHERE id=$1;
+        //         `,[req.body.getIdInput, req.body.getCountInput] )
+        //     client.query(`
+        //          INSERT INTO orders (itemId, count, userName) values
+        //         ( (SELECT id FROM items WHERE id=$1), $2, '${loggedIn}')
+        //          on conflict (itemId) DO
+        //          UPDATE SET
+        //          count = orders.count + $2
+        //          returning *;
+        //          `,
+        //         [req.body.getIdInput, req.body.getCountInput])
+        //     done()
+        //     res.redirect("/basket")
+        // })
+        n++
+    localStorage.setItem(`order${n}`, JSON.stringify({
+        'itemId': req.body.getIdInput,
+        'count': req.body.getCountInput
+    }))
+
+    console.log(`order${n}: ` +  (localStorage.getItem(`order${n}`)))
+
+    res.redirect('/basket')
+
     }
 )
 app.post('/basket/:id', (req,res)=>{
@@ -228,18 +232,60 @@ app.post('/signin', (req, res) => {
     // set the cookie as the token string, with a similar max age as the token
     // here, the max age is in milliseconds, so we multiply by 1000
     res.cookie('token', token, { maxAge: jwtExpirySeconds * 1000 })
-    res.redirect('/')
+    res.redirect('/buy')
     res.end()
 })
 app.get('/login', (req,res)=>{
     res.render('login', {layout:'mainLayout'})
 })
 app.post('/logout', (req,res)=>{
+    loggedIn=null
     res.clearCookie('token');
     res.redirect('/')
 })
 
-app.post('/buy', (req,res)=>{
+app.post('/buy', async (req,res)=>{
+    // We can obtain the session token from the requests cookies, which come with every request
+    const token = req.cookies.token
+
+    // if the cookie is not set, return an unauthorized error
+    if (!token) {
+        res.send('<a href="/login">Please log in</a>')
+    }
+
+    var payload
+    try {
+        payload = jwt.verify(token, jwtKey)
+    } catch (e) {
+        if (e instanceof jwt.JsonWebTokenError) {
+            // if the error thrown is because the JWT is unauthorized, return a 401 error
+            console.log(1)
+            return res.status(401).end()
+        }
+        // otherwise, return a bad request error
+        console.log(2)
+        return res.status(400).end()
+    }
+
+    // Finally, return the welcome message to the user, along with their
+    // username given in the token
+    loggedIn = payload.username
+    //res.send(`Welcome ${payload.username}!`)
+    pool.connect((err, client, done)=>{
+        if(err) return console.error(err)
+
+        client.query(`               
+                 UPDATE orders
+                 SET
+                 userName=REPLACE(
+                 userName, 
+                 'null',
+                 '${loggedIn}'//todo
+                 `)
+        done()
+        // res.redirect("/basket")
+        res.send(`all orders are by ${payload.username} purchased`)
+    })
 
 })
 app.listen(process.env.PORT || 3030)
